20e5466534562a5aad4ae6b60dc3aab82e60ccfa
Malkrich <mathurin.lemarie@gmail.com>
2024-10-11 13:20:20 +0200

    fixed C++ compiler version

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,8 @@
 project "spdlog"
     kind "StaticLib"
+    language "C++"
+    -- Must be compiled with C++11 (c.f. README.md)
+    cppdialect "C++11"
 
     targetdir("bin/" .. outputDir .. "/%{prj.name}")
     objdir("bin-int/" .. outputDir .. "/%{prj.name}")
